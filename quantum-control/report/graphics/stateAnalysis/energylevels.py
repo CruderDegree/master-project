bd885665296d29b5b72877f2192c9e3b1834577c
CruderDegree <tobi2109@gmail.com>
2020-11-12 10:42:30 +0100

    Fixed state analysis

--- quantum-control/report/graphics/stateAnalysis/energylevels.py
+++ quantum-control/report/graphics/stateAnalysis/energylevels.py
@@ -13,33 +13,33 @@ varE2 = []
 
 beta, E0, varE0, E1, varE1, E2, varE2 = fileReader("energies.txt", beta, E0, varE0, E1, varE1, E2, varE2, skiplines=1)
 
-E01 = E1.copy()
-E12 = E2.copy()
-E01Upper = [0] * len(varE0)
-E01Lower = [0] * len(varE0)
-E12Upper = [0] * len(varE0)
-E12Lower = [0] * len(varE0)
+E0Upper = [0] * len(varE0)
+E0Lower = [0] * len(varE0)
+E1Upper = [0] * len(varE0)
+E1Lower = [0] * len(varE0)
+E2Upper = [0] * len(varE0)
+E2Lower = [0] * len(varE0)
 
 for i in range(len(varE0)):
-    E01[i] -= E0[i]
-    E12[i] -= E1[i]
+    E0Upper[i] = E0[i] + sqrt(varE0[i])
+    E0Lower[i] = E0[i] - sqrt(varE0[i])
 
-    E01err = sqrt(varE0[i] + varE1[i])
-    E12err = sqrt(varE1[i] + varE2[i])
-    
-    E01Upper[i] = E01[i] + E01err
-    E01Lower[i] = E01[i] - E01err
+    E1Upper[i] = E1[i] + sqrt(varE1[i])
+    E1Lower[i] = E1[i] - sqrt(varE1[i])
 
-    E12Upper[i] = E12[i] + E12err
-    E12Lower[i] = E12[i] - E12err
+    E2Upper[i] = E2[i] + sqrt(varE2[i])
+    E2Lower[i] = E2[i] - sqrt(varE2[i])
 
-plt.fill_between(beta,E01Upper,E01Lower, alpha=0.25, color="b")
-plt.fill_between(beta,E12Upper,E12Lower, alpha=0.25, color="r")
-plt.plot(beta,E01,color="b",label=r"$E_1 - E_0$")
-plt.plot(beta,E12,color="r",label=r"$E_2 - E_1$")
+
+plt.fill_between(beta,E0Upper,E0Lower, alpha=0.25, color="b")
+plt.fill_between(beta,E1Upper,E1Lower, alpha=0.25, color="r")
+plt.fill_between(beta,E2Upper,E2Lower, alpha=0.25, color="g")
+plt.plot(beta,E0,color="b",label=r"$E_0$")
+plt.plot(beta,E1,color="r",label=r"$E_1$")
+plt.plot(beta,E2,color="g",label=r"$E_2$")
 plt.xlabel(r"$\beta$")
-plt.ylabel(r"$\Delta E$")
+plt.ylabel(r"Energy")
 plt.legend(loc="best")
 
-filename = "Energydifference"
+filename = "Energylevels"
 plt.savefig(filename+".pdf")
