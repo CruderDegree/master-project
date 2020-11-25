ec67e8599a03fdb8379c07ab5995b74d410732b4
Tobias Rasmussen <tobi2109@gmail.com>
2020-11-25 10:37:44 +0100

    Updated non-optimized control figure

---
+++ quantum-control/report/graphics/exploration/nonOptBeta.py
@@ -0,0 +1,24 @@
+"""
+REsulting fidelity of variying beta in a non-optimized solution in a quatic potential
+"""
+import matplotlib.pyplot as plt
+
+filename = "nonOptBeta"
+
+beta = []
+F = []
+data = open(filename+".txt", 'r')
+data.readline()
+line = data.readline()
+while(line):
+    ws = line.split('\t')
+    beta.append(float(ws[0]))
+    F.append(float(ws[1]))
+    line = data.readline()
+data.close()
+
+plt.plot(beta,F, 's-')
+plt.xlabel("β")
+plt.ylabel("Fidelity at t=T")
+
+plt.savefig(filename+".pdf")
